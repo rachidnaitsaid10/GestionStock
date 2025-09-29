0c9cce7ecdffe0374e6b066f299dff133f7b8262
OclaZ <aslikhhamza2001@gamil.com>
2025-09-29 14:52:52 +0100

    visualize final

---
+++ src/visualize.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from stats import nom, qte ,prix
+
+def bar_chart(nom,qte):
+    plt.bar(nom,qte , tick_label=nom , width=0.6)
+    plt.xlabel('nom des produit')
+    plt.ylabel('quantite')
+    plt.title('bar graph')
+    plt.show()
+
+def pie_chart(nom,qte,prix):
+    vp=qte*prix
+    plt.pie(vp,labels=nom)
+    plt.legend()
+    plt.show()
+
+
+
+
+
+bar_chart(nom,qte)
+pie_chart(nom,qte,prix)
+
+
+
