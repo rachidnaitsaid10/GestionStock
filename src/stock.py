33bf1da585c1b6c3b48b4c04339af3738d3ac7f7
rachidnaitsaid10 <rachidnaitsaid10@gmail.com>
2025-09-29 14:48:42 +0100

    crud of Product

--- src/stock.py
+++ src/stock.py
@@ -1,15 +1,69 @@
+import numpy as np
 from Data import data
-from numpy import np
 
 Products = data
+print(Products)
 
 def AddProduct():
     Nom = str(input("Entry Product Name :"))
     Qunt = int(input("Entre Quantité :"))
     Prix = float(input("Entre Prix de produit :"))
+    element = np.array([[Nom,Qunt,Prix]])
 
-    NewData = np.append(Products, [Nom, Qunt, Prix])
+    NewData = np.append(Products, element, axis=0)
+    print(np.array(NewData))
 
-    print(NewData)
 
-AddProduct()
+
+def RemoveProduct():
+    NomProduct = str(input("Entry Product Name :"))
+    index = np.where(Products == NomProduct)
+
+    if len(index[0]) == 0:
+        print("Product Not Found")
+    else:
+        NewData = np.delete(Products, index[0], axis=0)
+        print(NewData)
+
+# RemoveProduct()
+
+def UpdateProduct():
+    NomProduct = str(input("Entry Product Name :"))
+    index = np.where(Products == NomProduct)
+    if len(index[0]) == 0:
+        print("Product Not Found")
+    else:
+        Qunt = int(input("Entre Quantit :"))
+        Products[index[0], 1] = Qunt
+    print(Products)
+
+# UpdateProduct()
+
+
+def UpdateProduct():
+    NomProduct = str(input("Entry Product Name :"))
+    index = np.where(Products == NomProduct)
+    if len(index[0]) == 0:
+        print("Product Not Found")
+    else:
+        Qunt = int(input("Entre Quantit :"))
+        Products[index[0], 1] = Qunt
+    print(Products)
+
+# UpdateProduct()
+
+def DisplayProducts():
+    for product in range(len(Products)):
+        Element = Products[product]
+        print(f"Nom : {Element[0]} \nQunt : {Element[1]} \nPrix : {Element[2]} ")
+        print("-"*15)
+
+DisplayProducts()
+
+def DisplayProducts():
+    for product in range(len(Products)):
+        Element = Products[product]
+        print(f"Nom : {Element[0]} \nQunt : {Element[1]} \nPrix : {Element[2]} ")
+        print("-"*15)
+
+DisplayProducts()
